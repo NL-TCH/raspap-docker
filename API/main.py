22886a6b1390cee003088f2dac3b56d030e92d61
NL-TCH <teunish@outlook.com>
2024-01-20 22:15:57 +0100

    AP + basic networking

--- API/main.py
+++ API/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from fastapi.security.api_key import APIKey
 
+import json
+
 import system
+import networking
+import ap
 import restart
 
 app = FastAPI()
@@ -21,6 +25,35 @@ async def get_system():
 'operatingSystem': system.operatingSystem(),
 'kernelVersion': system.kernelVersion(),
 'rpiRevision': system.rpiRevision()
+}
+
+@app.get("/ap")
+async def get_ap():
+    return{
+'driver': ap.driver(),
+'ctrl_interface': ap.ctrl_interface(),
+'ctrl_interface_group': ap.ctrl_interface_group(),
+'auth_algs': ap.auth_algs(),
+'wpa_key_mgmt': ap.wpa_key_mgmt(),
+'beacon_int': ap.beacon_int(),
+'ssid': ap.ssid(),
+'channel': ap.channel(),
+'hw_mode': ap.hw_mode(),
+'ieee80211n': ap.ieee80211n(),
+'wpa_passphrase': ap.wpa_passphrase(),
+'interface': ap.interface(),
+'wpa': ap.wpa(),
+'wpa_pairwise': ap.wpa_pairwise(),
+'country_code': ap.country_code(),
+'ignore_broadcast_ssid': ap.ignore_broadcast_ssid()
+}
+
+
+@app.get("/networking")
+async def get_networking():
+    return{
+'interfaces': json.loads(networking.interfaces()),
+'throughput': json.loads(networking.throughput())
 }
 
 @app.post("/restart/webgui")
